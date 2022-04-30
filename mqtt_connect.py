dee8b39352ec499cd633d4f6aa93d892458e72bb
Vanessa <vanessadcamara@gmail.com>
2022-04-30 18:00:30 -0400

    adc atualizações

--- mqtt_connect.py
+++ mqtt_connect.py
@@ -13,7 +13,7 @@ topic = "python/mqtt"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 # username = 'emqx'
-# password = 'public'
+# password = 'public
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -28,36 +28,35 @@ def connect_mqtt():
     client.connect(broker, port)
     return client
 
-
-def publish(client):
-    msg_count = 0
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
+client = connect_mqtt()
 
 
-def run():
-    # client = connect_mqtt()
-    # client.loop_start()
-    # publish(client)
+def publish(topic, msg):
+    result = client.publish(topic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        # print(f"Send to topic `{topic}`")
+        return 1
+    else:
+        # print(f"Failed to send message to topic {topic}")
+        return 0
 
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
+# def run():
+#     # publish
+#     # # client.loop_start()
+#     # publish(client)
+    
+#     # subscribe
+#     subscribe()
+#     client.loop_start()
 
-def subscribe(client: mqtt_client):
+def subscribe(topic):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        return msg.payload.decode()
     client.subscribe(topic)
     client.on_message = on_message
 
-if __name__ == '__main__':
-    run()
+# if __name__ == '__main__':
+    # run()
