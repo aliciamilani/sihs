3959228ce47878f49ccd66c3db153cb0a7dec0fc
jordanjose0410 <jordanjose0410@gmail.com>
2022-05-03 18:18:10 -0400

    sensores sendo lidos e motor sendo ativado

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import mqtt_connect
+import time
 from weather_api import get_weather
+from step_motor import setStepMotor
+from sensor_dht11 import dht11_read
+from relay import turn_on, turn_off
 
 # API
 
-response = get_weather()
+#response = get_weather()
 
-mqtt_connect.publish("est/si/sihs/weather", response)
+#mqtt_connect.publish("est/si/sihs/weather", response)
 
+for i in range(10):
+    tempo = get_weather()
+    print(f"Tempo:{tempo}")
+    print("Motor girando sentido horario")
+    setStepMotor(512, 1)
+    temp_hum = dht11_read.read_temp_humidity()
+    if temp_hum:
+        print(f"Temperatura: {temp_hum[0]}C")
+        print(f"Umidade: {temp_hum[1]}%")
+    print("Ligando Rele")
+    turn_on()
+    time.sleep(1)
+    print("desligando rele")
+    turn_off()
--- relay.py
+++ relay.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
-RELAIS_1_GPIO = 16
+RELAIS_1_GPIO = 24
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)
 
 def turn_on():
@@ -11,12 +11,11 @@ def turn_on():
 def turn_off():
     GPIO.output(RELAIS_1_GPIO, False)
 
-for i in range(2):
-    print('liga')
-    turn_on() # on
-    time.sleep(1)
-    print('desliga')
-    turn_off()
-    time.sleep(1)
+#for i in range(10):
+    #print('liga')
+    ##ime.sleep(1)
+    #print('desliga')
+    #turn_off()
+    #time.sleep(1)
 
-GPIO.cleanup()
+#GPIO.cleanup()
