6a4be3f56e8fb534e339ad7561c1cd02938beac7
Vanessa <vanessadcamara@gmail.com>
2022-05-07 16:06:01 -0400

    feat: added insert into main

--- database/insert_table.py
+++ database/insert_table.py
@@ -9,6 +9,7 @@ def insert_climates(climate_type):
                             '{climate_type}'
                         )''')
 
+
 def insert_users(user_name, user_email, user_password):
     cursor.execute(f''' INSERT INTO users (
                             user_name,
@@ -19,6 +20,7 @@ def insert_users(user_name, user_email, user_password):
                             '{user_email}',
                             '{user_password}'
                         )''')
+
 
 def insert_historics(history_device, history_action, history_date, history_time, 
                     history_humidity, history_temperature, history_climate_id, history_user_id):
@@ -42,15 +44,35 @@ def insert_historics(history_device, history_action, history_date, history_time,
                             {history_user_id}
                         )''')
 
+def search_climate (climate):
+    cursor.execute(f''' SELECT climate_id FROM climates WHERE climate_type = '{climate}' ''')
+    return cursor.fetchall()[0][0]
+
+conn.commit()
+cursor.close()
+conn.close()
+
 # import datetime
 # current = datetime.datetime.now() 
 
 # insert_climates("Clouds")
-# insert_users("Anna", "aliciamilani92@gmail.com", "123456")
-# insert_historics("Relay", "On", current.date(), current.time(), 
-#                     94, 26, 1, 1)
+# insert_users("Vanessa", "vanessadcamara@gmail.com", "654321")
+# insert_historics("Relay", "On", current.date(), current.time(), 94, 26, 1, 1)
+# insert_climates("Thunderstorm")
+# insert_climates("Drizzle")
+# insert_climates("Rain")
+# insert_climates("Squall")
+# insert_climates("Snow")
+# insert_climates("Mist")
+# insert_climates("Smoke")
+# insert_climates("Haze")
+# insert_climates("Dust")
+# insert_climates("Fog")
+# insert_climates("Sand")
+# insert_climates("Dust")
+# insert_climates("Ash")
+# insert_climates("Squall")
+# insert_climates("Tornado")
+# insert_climates("Clear")
 
 
-# conn.commit()
-# cursor.close()
-# conn.close()
